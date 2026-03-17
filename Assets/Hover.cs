e6234f901b5efabf5dfff7148282bc98148fd8fd
Lino-coder01 <line.hajjarr@gmail.com>
2026-03-17 14:12:50 +0100

    Initial commit

---
+++ Assets/Hover.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class OarHighlight : MonoBehaviour
+{
+    public Renderer oarRenderer;
+    public Color normalColor = Color.white;
+    public Color hoverColor = Color.cyan;
+    public Color grabbedColor = Color.yellow;
+
+    private XRSimpleInteractable interactable;
+
+    void Start()
+    {
+        interactable = GetComponent<XRSimpleInteractable>();
+
+        // Hover
+        interactable.hoverEntered.AddListener(OnHover);
+        interactable.hoverExited.AddListener(OnHoverExit);
+
+        // Grab
+        interactable.selectEntered.AddListener(OnGrab);
+        interactable.selectExited.AddListener(OnRelease);
+    }
+
+    void OnHover(HoverEnterEventArgs args)
+    {
+        oarRenderer.material.color = hoverColor;
+    }
+
+    void OnHoverExit(HoverExitEventArgs args)
+    {
+        oarRenderer.material.color = normalColor;
+    }
+
+    void OnGrab(SelectEnterEventArgs args)
+    {
+        oarRenderer.material.color = grabbedColor;
+    }
+
+    void OnRelease(SelectExitEventArgs args)
+    {
+        oarRenderer.material.color = normalColor;
+    }
+}
